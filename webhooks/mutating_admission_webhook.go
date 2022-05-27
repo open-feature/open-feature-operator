8a848d5ff17887cbe6466c934e848ea4dcad9b7d
Alex Jones <alexsimonjones@gmail.com>
2022-05-27 14:54:01 +0100

    Bug/blocked pods (#9)
    * improved getting started steps
    
    * fixed self signed issuer reference
    
    * fixed bug with pods being blocked for non openfeature workloads

--- webhooks/mutating_admission_webhook.go
+++ webhooks/mutating_admission_webhook.go
@@ -17,7 +17,7 @@ import (
 // NOTE: RBAC not needed here.
 //+kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:webhook:path=/mutate-v1-pod,mutating=true,failurePolicy=Ignore,groups="",resources=pods,verbs=create;update,versions=v1,name=mpod.kb.io,admissionReviewVersions=v1,sideEffects=NoneOnDryRun
+// +kubebuilder:webhook:path=/mutate-v1-pod,mutating=true,failurePolicy=Ignore,groups="",resources=pods;deployments,verbs=create;update,versions=v1,name=mpod.kb.io,admissionReviewVersions=v1,sideEffects=NoneOnDryRun
 
 // PodMutator annotates Pods
 type PodMutator struct {
@@ -39,11 +39,11 @@ func (m *PodMutator) Handle(ctx context.Context, req admission.Request) admissio
 	// Check enablement
 	val, ok := pod.GetAnnotations()["openfeature.dev"]
 	if !ok {
-		return admission.Response{}
+		return admission.Allowed("no annotation")
 	} else {
 		if val != "enabled" {
 			m.Log.V(2).Info("openfeature.dev Annotation is not enabled")
-			return admission.Response{}
+			return admission.Allowed("openfeature is disabled")
 		}
 	}
 	var featureFlagCustomResource corev1alpha1.FeatureFlagConfiguration
@@ -81,7 +81,9 @@ func (m *PodMutator) Handle(ctx context.Context, req admission.Request) admissio
 			"config.yaml": featureFlagCustomResource.Spec.FeatureFlagSpec,
 		},
 	}); err != nil {
-		fmt.Printf(fmt.Sprintf("failed to create config map %s", configName))
+
+		m.Log.V(1).Info(fmt.Sprintf("failed to create config map %s", configName))
+
 		return admission.Errored(http.StatusInternalServerError, err)
 	}
 
