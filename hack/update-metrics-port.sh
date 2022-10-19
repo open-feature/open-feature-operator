13047867732c3a4e98417b6c8420879352cbe725
jamesmilligan <james@omnant.co.uk>
2022-10-19 14:09:26 +0100

    updated config map generator
    Signed-off-by: jamesmilligan <james@omnant.co.uk>

--- hack/update-metrics-port.sh
+++ hack/update-metrics-port.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 METRICS_PORT=$1
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    sed -i "s/INPUT_METRICS_PORT/${METRICS_PORT}/g" config/manager/kustomization.yaml
+    sed -i "s/INPUT_FLAGD_METRIC_PORT/${METRICS_PORT}/g" config/manager/kustomization.yaml
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	sed -i '' -e "s/INPUT_METRICS_PORT/${METRICS_PORT}/g" config/manager/kustomization.yaml
+	sed -i '' -e "s/INPUT_FLAGD_METRIC_PORT/${METRICS_PORT}/g" config/manager/kustomization.yaml
 fi
