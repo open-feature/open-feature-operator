2c1ab9c7286e97fcb41b3c6ffc0322d2abbfb3f9
jamesmilligan <james@omnant.co.uk>
2022-11-02 10:46:44 +0000

    added replace path for chart/templates/deployment.yaml
    Signed-off-by: jamesmilligan <james@omnant.co.uk>

--- hack/update-flagd.sh
+++ hack/update-flagd.sh
@@ -2,8 +2,8 @@
 FLAGD_VERSION=$1
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sed -i "s/INPUT_FLAGD_VERSION/${FLAGD_VERSION}/g" config/manager/manager.yaml
-    sed -i "s/INPUT_FLAGD_VERSION/${FLAGD_VERSION}/g" chart/template/deployment.yaml
+    sed -i "s/INPUT_FLAGD_VERSION/${FLAGD_VERSION}/g" chart/templates/deployment.yaml
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	sed -i '' -e "s/INPUT_FLAGD_VERSION/${FLAGD_VERSION}/g" config/manager/manager.yaml
-    sed -i '' -e "s/INPUT_FLAGD_VERSION/${FLAGD_VERSION}/g" chart/template/deployment.yaml
+    sed -i '' -e "s/INPUT_FLAGD_VERSION/${FLAGD_VERSION}/g" chart/templates/deployment.yaml
 fi
