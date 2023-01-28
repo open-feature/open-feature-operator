8cb43eb012a20f493a8510086bbdabc15ea0efaa
Todd Baert <toddbaert@gmail.com>
2023-01-27 23:22:59 -0500

    fixup: tests
    Signed-off-by: Todd Baert <toddbaert@gmail.com>

--- webhooks/pod_webhook_test.go
+++ webhooks/pod_webhook_test.go
@@ -455,7 +455,7 @@ var _ = Describe("pod mutation webhook", func() {
 		Expect(pod.Spec.Containers[1].Args).To(Equal([]string{
 			"start",
 			"--uri",
-			"file:/etc/flagd/test-mutate-pod_test-feature-flag-configuration.json",
+			"file:/etc/flagd/test-mutate-pod_test-feature-flag-configuration/test-mutate-pod_test-feature-flag-configuration.json",
 			"--sync-provider-args",
 			"key=value",
 			"--sync-provider-args",
