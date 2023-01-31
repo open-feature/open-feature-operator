f3fe057e180528944110a6841239d3f9d471086b
Skye Gill <gill.skye95@gmail.com>
2023-01-31 16:56:38 +0000

    chore: test multiple sync providers (#325)
    Signed-off-by: Skye Gill <gill.skye95@gmail.com>

--- test/e2e/tests/001.flagd-response.sh
+++ test/e2e/tests/001.flagd-response.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./"$(dirname "${BASH_SOURCE[0]}")"/../simple-flag-evaluation.sh '{"value":true,"reason":"STATIC","variant":"on"}'
+./"$(dirname "${BASH_SOURCE[0]}")"/../flag-evaluation.sh simple-flag '{"value":true,"reason":"STATIC","variant":"on"}'
+./"$(dirname "${BASH_SOURCE[0]}")"/../flag-evaluation.sh simple-flag-filepath '{"value":true,"reason":"STATIC","variant":"on"}'
+./"$(dirname "${BASH_SOURCE[0]}")"/../flag-evaluation.sh simple-flag-filepath2 '{"value":true,"reason":"STATIC","variant":"on"}'
