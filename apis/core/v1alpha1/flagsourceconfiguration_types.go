d02478a1df70702087bc31351f750149164d3ee6
James Milligan <james@omnant.co.uk>
2023-02-13 09:59:14 +0000

    cleanup
    Signed-off-by: James Milligan <james@omnant.co.uk>

--- apis/core/v1alpha1/flagsourceconfiguration_types.go
+++ apis/core/v1alpha1/flagsourceconfiguration_types.go
@@ -46,8 +46,8 @@ const (
 	defaultSocketPath          string = ""
 	defaultEvaluator           string = "json"
 	defaultImage               string = "ghcr.io/open-feature/flagd"
-	// `v0.3.4` is replaced in the `update-flagd` Makefile target
-	defaultTag             string           = "v0.3.4"
+	// `INPUT_FLAGD_VERSION` is replaced in the `update-flagd` Makefile target
+	defaultTag             string           = "INPUT_FLAGD_VERSION"
 	SyncProviderKubernetes SyncProviderType = "kubernetes"
 	SyncProviderFilepath   SyncProviderType = "filepath"
 	SyncProviderHttp       SyncProviderType = "http"
