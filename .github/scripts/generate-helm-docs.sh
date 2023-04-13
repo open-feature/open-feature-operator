fe1721b652f4941ea8a1a02f47bef82f9b7a0a53
odubajDT <ondrej.dubaj@dynatrace.com>
2023-04-13 07:49:57 +0200

    ci: introduce workflow for helm documentation validation
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

---
+++ .github/scripts/generate-helm-docs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Readme generator for OpenFeature Operator Helm Chart
+#
+# This script will install the readme generator if it's not installed already
+# and then it will generate the README.md file from the local Helm values
+#
+# Dependencies:
+# Node >=16
+
+# renovate: datasource=github-releases depName=bitnami-labs/readme-generator-for-helm
+GENERATOR_VERSION="2.5.0"
+
+echo "Checking if readme generator is installed already..."
+if [[ $(npm list -g | grep -c "readme-generator-for-helm@${GENERATOR_VERSION}") -eq 0 ]]; then
+  echo "Readme Generator v${GENERATOR_VERSION} not installed, installing now..."
+  git clone https://github.com/bitnami-labs/readme-generator-for-helm.git
+  cd ./readme-generator-for-helm || exit
+  git checkout ${GENERATOR_VERSION}
+  npm ci
+  cd ..
+  npm install -g ./readme-generator-for-helm
+else
+  echo "Readme Generator is already installed, continuing..."
+fi
+
+echo "Generating readme now..."
+cat ./chart/open-feature-operator/values.yaml ./chart/open-feature-operator/doc.yaml > ./chart/open-feature-operator/rendered.yaml
+readme-generator --config $(pwd)/chart/open-feature-operator/helm-docs-config.json --values=./chart/open-feature-operator/rendered.yaml --readme=./chart/open-feature-operator/README.md
+
+# Please be aware, the readme file needs to exist and needs to have a Parameters section, as only this section will be re-generated
