50a0f1b2a3e95ad3090f34348cada16885181fc5
Kavindu Dodanduwa <kavindudodanduwa@gmail.com>
2023-08-01 08:47:22 -0700

    fix e2e tests
    Signed-off-by: Kavindu Dodanduwa <kavindudodanduwa@gmail.com>

--- test/e2e/tests/001.flagd-response.sh
+++ test/e2e/tests/001.flagd-response.sh
@@ -3,10 +3,10 @@
 FAILURE=0
 
 flagKeys=('simple-flag' 'simple-flag-filepath' 'simple-flag-filepath2')
-expectedFlagResponses=('{"value":true,"reason":"STATIC","variant":"on"}' '{"value":true,"reason":"STATIC","variant":"on"}' '{"value":true,"reason":"STATIC","variant":"on"}')
+expectedResponseContain=('value":true,"reason":"STATIC","variant":"on"' '"value":true,"reason":"STATIC","variant":"on"' '"value":true,"reason":"STATIC","variant":"on"')
 
 for i in "${!flagKeys[@]}"; do
-  ./"$(dirname "${BASH_SOURCE[0]}")"/../flag-evaluation.sh "${flagKeys[$i]}" "${expectedFlagResponses[$i]}"
+  ./"$(dirname "${BASH_SOURCE[0]}")"/../flag-evaluation.sh "${flagKeys[$i]}" "${expectedResponseContain[$i]}"
   EXIT_CODE=$?
   if [ $EXIT_CODE -ne 0 ];
     then
