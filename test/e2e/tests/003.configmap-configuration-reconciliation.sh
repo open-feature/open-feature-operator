4e558048ecdbc21d6eca299a5edf2fbbd3c19629
Michael Beemer <beeme1mr@users.noreply.github.com>
2023-08-07 16:52:29 -0400

    update expected value to include default values
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>

--- test/e2e/tests/003.configmap-configuration-reconciliation.sh
+++ test/e2e/tests/003.configmap-configuration-reconciliation.sh
@@ -19,7 +19,7 @@ spec:
 EOF
 
 # filepath sync provider takes up to 2 minutes to synchronize, retry every 10 seconds up to 12 times
-./"$(dirname "${BASH_SOURCE[0]}")"/../flag-evaluation.sh simple-flag-filepath '{"reason":"STATIC","variant":"off"}' 12 10
+./"$(dirname "${BASH_SOURCE[0]}")"/../flag-evaluation.sh simple-flag-filepath '{'value":false,"reason":"STATIC","variant":"off"}' 12 10
 EXIT_CODE=$?
 
 kubectl -n open-feature-operator-system apply -f ./test/e2e/e2e.yml > /dev/null # reset state quietly
