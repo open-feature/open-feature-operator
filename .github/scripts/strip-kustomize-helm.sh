b84c1a7bfc24756304b400cd955241288913ce5d
Todd Baert <todd.baert@dynatrace.com>
2024-06-05 17:19:17 -0400

    fixup: use ___ instead of --
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- .github/scripts/strip-kustomize-helm.sh
+++ .github/scripts/strip-kustomize-helm.sh
@@ -2,17 +2,17 @@
 
 # This script is a hack to support helm flow control in kustomize overlays, which would otherwise break them.
 # It allows us to render helm template bindings and add newlines.
-# For instance, it transforms "__{{ .Value.myValue }}__" to {{ .Value.myValue }}.
-# It also adds newlines wherever __newline__ is found.
+# For instance, it transforms "___{{ .Value.myValue }}___" to {{ .Value.myValue }}.
+# It also adds newlines wherever ___newline___ is found.
 
 CHARTS_DIR='./chart/open-feature-operator/templates';
 
 echo 'Running strip-kustomize-helm.sh script'
 filenames=`find $CHARTS_DIR -name "*.yaml"`
 for file in $filenames; do
-    sed -i "s/__newline__/\\n/g" $file
-    sed -i "s/\"__//g" $file
-    sed -i "s/__\"//g" $file
-    sed -i "s/__//g" $file
+    sed -i "s/___newline___/\\n/g" $file
+    sed -i "s/\"___//g" $file
+    sed -i "s/___\"//g" $file
+    sed -i "s/___//g" $file
 done
 echo 'Done running strip-kustomize-helm.sh script'
