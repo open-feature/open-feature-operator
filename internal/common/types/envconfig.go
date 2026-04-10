open-feature/open-feature-operator

commit 0668bd63998d8a0e88e0ccdafbc2b5fbb549c0c5

fix(envconfig): rename CLUSTER_DOMAIN env var to FLAGD_CLUSTER_DOMAIN (#792)
Signed-off-by: Roman Kuznetsov <roku@lix.one>

internal/common/types/envconfig.go
@@ -10,7 +10,7 @@ type EnvConfig struct {
 	FlagdProxyPort           int    `envconfig:"FLAGD_PROXY_PORT" default:"8015"`
 	FlagdProxyManagementPort int    `envconfig:"FLAGD_PROXY_MANAGEMENT_PORT" default:"8016"`
 	FlagdProxyDebugLogging   bool   `envconfig:"FLAGD_PROXY_DEBUG_LOGGING" default:"false"`
-	ClusterDomain            string `envconfig:"CLUSTER_DOMAIN" default:"cluster.local"`
+	FlagdClusterDomain       string `envconfig:"FLAGD_CLUSTER_DOMAIN" default:"cluster.local"`
 
 	FlagdImage string `envconfig:"FLAGD_IMAGE" default:"ghcr.io/open-feature/flagd"`
 	// renovate: datasource=github-tags depName=open-feature/flagd/flagd

internal/common/flagdproxy/flagdproxy.go
@@ -62,7 +62,7 @@ func NewFlagdProxyConfiguration(env types.EnvConfig, imagePullSecrets []string, 
 		ImagePullSecrets:       imagePullSecrets,
 		Labels:                 labels,
 		Annotations:            annotations,
-		ClusterDomain:          env.ClusterDomain,
+		ClusterDomain:          env.FlagdClusterDomain,
 	}
 }
 

internal/common/flagdproxy/flagdproxy_test.go
@@ -205,7 +205,7 @@ func TestNewFlagdProxyConfiguration(t *testing.T) {
 		FlagdProxyPort:           8015,
 		FlagdProxyManagementPort: 8016,
 		FlagdProxyReplicaCount:   123,
-		ClusterDomain:            "cluster.local",
+		FlagdClusterDomain:       "cluster.local",
 	}, pullSecrets, labels, annotations)
 
 	require.NotNil(t, kpConfig)

commit 3cf6a6a0ecf273b5b667fc89c061462f94a754c1

fix!: rename CLUSTER_DOMAIN env var to FLAGD_CLUSTER_DOMAIN
Documents the breaking env var rename from CLUSTER_DOMAIN to
FLAGD_CLUSTER_DOMAIN introduced in #792.

BREAKING CHANGE: CLUSTER_DOMAIN env var is now FLAGD_CLUSTER_DOMAIN.

Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

internal/common/types/envconfig.go
@@ -10,7 +10,8 @@ type EnvConfig struct {
 	FlagdProxyPort           int    `envconfig:"FLAGD_PROXY_PORT" default:"8015"`
 	FlagdProxyManagementPort int    `envconfig:"FLAGD_PROXY_MANAGEMENT_PORT" default:"8016"`
 	FlagdProxyDebugLogging   bool   `envconfig:"FLAGD_PROXY_DEBUG_LOGGING" default:"false"`
-	FlagdClusterDomain       string `envconfig:"FLAGD_CLUSTER_DOMAIN" default:"cluster.local"`
+	// renamed from CLUSTER_DOMAIN to FLAGD_CLUSTER_DOMAIN in 0.8.10
+	FlagdClusterDomain string `envconfig:"FLAGD_CLUSTER_DOMAIN" default:"cluster.local"`
 
 	FlagdImage string `envconfig:"FLAGD_IMAGE" default:"ghcr.io/open-feature/flagd"`
 	// renovate: datasource=github-tags depName=open-feature/flagd/flagd